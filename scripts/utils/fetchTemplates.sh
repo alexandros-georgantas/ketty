bf653ff91ce7f0d30c1be292f1e72baae361c5cd
Alexandros Georgantas <alexgeorg86@gmail.com>
2022-04-04 12:13:17 +0000

    fix of auto template creation

--- scripts/utils/fetchTemplates.sh
+++ scripts/utils/fetchTemplates.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -e
 # List of all the templates
-[ -e draft ] || git clone https://gitlab.pagedmedia.org/pagedjs-templates/atla.git ./templates/atla
+[ -e draft ] || git clone https://gitlab.coko.foundation/pagedjs/pagedjs-templates/atla.git ./templates/atla
