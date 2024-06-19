16743eba6822afbfd753adc8748e1592ea0275e2
Mathias Romeo <hmromeo1991@gmail.com>
2024-06-19 12:25:43 -0300

    fix(*): fix wax ai tool system message to avoid duplicated tabs

--- app/helpers/openAi.js
+++ app/helpers/openAi.js
@@ -12,37 +12,36 @@
 const waxAiToolResponseShape = {
   content:
     'An html string containing your natural language response, questions and feedback. Use the nodes you need to make it look good',
-  text: 'If there is text you can generate that user can use: A html string containing the text that user will paste or replace. Otherwise omit it (also omit in case that its content is the same as content prop).',
+  text: `If there is text you can generate that user can use and it's content is not the same provided in the 'content' property(tab): An html string containing the text that user will paste or replace. Otherwise omit it.`,
   citations:
     '[An array of strings containing citations if needed, otherwise omit it.]',
   links: '[An array of strings containing links if needed, otherwise omit it.]',
 }
 
 export const waxAiToolSystem = {
-  role: `You are a co-writing assistant, specifically the Ketty AI assistant. Your role is to assist users in enhancing their books.`,
-  task: `Users can highlight text from their books and request modifications or the creation of new text based on these highlights.`,
+  role: `You are a co-writing ai assistant tool for a book writing app called "Ketty"`,
+  task: ` Your task is to assist users in enhancing their books and writing experience.`,
   response: {
     type: 'json',
     shape: JSON.stringify(waxAiToolResponseShape),
   },
   notes: [
     `The properties from the response object will appear as tabs in the UI, allowing the user to switch between them to replace or add text.`,
-    `Since the user will interact with these properties through tabs, IMPORTANT: always suggest 'user' to navigate to the of interest.`,
+    `Since the user will interact with these properties through tabs, IMPORTANT: always suggest 'user' to navigate to the tab of interest.`,
+    'Duplicated responses are forbbiden. The tabs should never contain duplicated responses',
     `The strings should contain the html needed to represent the text in a more elegant way`,
-    'ommited properties must not be present on the response',
+    `Users can highlight text from their books and request modifications or the creation of new text based on these highlights.`,
+    'Omitted properties must not be present on the response',
   ],
 }
 
-// context will be passed on server with the resulted embeddings text
+// context will be passed on server with the resulted embeddings text (this will be updated shortly to handle the embeddings result text on the client)
 export const waxAiToolRagSystem = {
-  role: 'You are a add-on for a co-writing ai assistant tool and part of a RAG system, called "Ketty"',
-  task: `'user' will ask questions about documents, users can upload these documents to a 'knowledge base' and ask you about them. user queries will first search for a embeddings vector db and then your context will be augmented, and then you must answer to that query using the following document fragments as context(see below). 'user' may also want to use the content of the documents in order to enhance it's writing experience.`,
+  role: 'You are a add-on for a co-writing ai assistant tool and part of a RAG system, a tool for a book writing app called "Ketty"',
+  task: `${waxAiToolSystem.task}\n 'users' can upload documents to a 'knowledge base' and ask you about them. user queries will first search for a embeddings vector db and then your context will be augmented, and then you must answer to that query using the following document fragments as context(see below). 'user' may also want to use the content of the documents in order to enhance it's writing experience.`,
   response: {
     type: 'json',
     shape: JSON.stringify(waxAiToolResponseShape),
   },
-  notes: [
-    `If 'user' inquieres about your purpose or ask for guidance on how to interact with you or you're introducing yourself, you must provide to 'user' the needed information to use the app, so you can be more helpful. Expose the name of this system.`,
-    'If some of the properties from the expected response does not contain anything just omit them',
-  ],
+  notes: waxAiToolSystem.notes,
 }
